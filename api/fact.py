dc31e5951a287710fed6c9fd170ce53632065308
vardaansinha <vardsin28@gmail.com>
2023-02-15 21:49:12 -0800

    squiggly doo

--- api/fact.py
+++ api/fact.py
@@ -3,10 +3,13 @@ from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 from model.facts import FactofDay
 
+
 fact_api = Blueprint('fact_api', __name__, url_prefix='/api/fact')
+
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(fact_api)
+
 
 class factAPI:        
     class _Create(Resource):
@@ -19,29 +22,32 @@ class factAPI:
              fact = body.get('fact')
              if fact is None or len(fact) < 2:
                 return {'message': f'Fact is missing, or is less than 2 characters'}, 210
-            
-            # look for score, type
-             score = body.get('score')
-             type = body.get('type')
+           
+            # look for date, year
+             date = body.get('date')
+             year = body.get('year')
 
-            
-             uo = fact(facts=fact, score=score, type=type)
-            
+
+           
+             uo = FactofDay(fact, date, year)
+           
              ''' Additional garbage error checking '''
-            
-            
+           
+           
              fact = uo.create()
-            
+           
              if fact:
                 return jsonify(fact.read())
             # failure returns error
              return {'message': f'Processed news error'}, 210
+
 
     class _Read(Resource):
         def get(self):
             facts = FactofDay.query.all()    # read/extract all users from database
             json_ready = [fact.read() for fact in facts]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+
 
     # building RESTapi endpoint
     api.add_resource(_Create, '/create')
