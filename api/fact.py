86b42230ed6e2442d2077b24ad6acc847704e7f4
vardaansinha <vardsin28@gmail.com>
2023-02-20 19:20:36 -0800

    trying api changes

--- api/fact.py
+++ api/fact.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
-from flask_restful import Api, Resource # used for REST API building
+from flask_restful import Api, Resource, reqparse # used for REST API building
 from datetime import datetime
 from model.facts import FactofDay
+from __init__ import db
 
 
 fact_api = Blueprint('fact_api', __name__, url_prefix='/api/fact')
@@ -40,6 +41,40 @@ class factAPI:
                 return jsonify(fact.read())
             # failure returns error
              return {'message': f'Processed news error'}, 210
+        def put(self):
+            parser = reqparse.RequestParser()
+            parser.add_argument("fact", required=True, type=str)
+            parser.add_argument("date", required=True, type=str)
+            parser.add_argument("year", required=True, type=int)
+            args = parser.parse_args()
+
+            try:
+                fact = db.session.query(FactofDay).get(args["a"])
+                if fact:
+                    db.session.commit()
+                else:
+                    return {"message": "fact not found"}, 404
+            except Exception as e:
+                db.session.rollback()
+                return {"message": f"server error: {e}"}, 500
+
+        def delete(self):
+            parser = reqparse.RequestParser()
+            parser.add_argument("fact", required=True, type=str)
+            args = parser.parse_args()
+
+            try:
+                fact = db.session.query(FactofDay).get(args["a"])
+                if fact:
+                    db.session.delete(fact)
+                    db.session.commit()
+                    return fact.to_dict()
+                else:
+                    return {"message": "subscription not found"}, 404
+            except Exception as e:
+                db.session.rollback()
+                return {"message": f"server error: {e}"}, 500
+
 
 
     class _Read(Resource):
@@ -47,6 +82,7 @@ class factAPI:
             facts = FactofDay.query.all()    # read/extract all users from database
             json_ready = [fact.read() for fact in facts]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+        
 
 
     # building RESTapi endpoint
