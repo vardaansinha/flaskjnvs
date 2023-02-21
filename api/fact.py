7c66f09fb31255fda671c676fafd6a71ef8d19b1
vardaansinha <vardsin28@gmail.com>
2023-02-20 22:17:01 -0800

    fix

--- api/fact.py
+++ api/fact.py
@@ -21,8 +21,8 @@ class factAPI:
              ''' Avoid garbage in, error checking '''
             # validate name
              fact = body.get('fact')
-             if fact is None or len(fact) < 2:
-                return {'message': f'Fact is missing, or is less than 2 characters'}, 210
+             if fact is None or len(fact) < 13:
+                return {'message': f'Fact is missing'}, 210
            
             # look for date, year
              date = body.get('date')
@@ -40,43 +40,8 @@ class factAPI:
              if fact:
                 return jsonify(fact.read())
             # failure returns error
-             return {'message': f'Processed news error'}, 210
-        def put(self):
-            parser = reqparse.RequestParser()
-            parser.add_argument("fact", required=True, type=str)
-            parser.add_argument("date", required=True, type=str)
-            parser.add_argument("year", required=True, type=int)
-            args = parser.parse_args()
-
-            try:
-                fact = db.session.query(FactofDay).get(args["a"])
-                if fact:
-                    db.session.commit()
-                else:
-                    return {"message": "fact not found"}, 404
-            except Exception as e:
-                db.session.rollback()
-                return {"message": f"server error: {e}"}, 500
-
-        def delete(self):
-            parser = reqparse.RequestParser()
-            parser.add_argument("fact", required=True, type=str)
-            args = parser.parse_args()
-
-            try:
-                fact = db.session.query(FactofDay).get(args["a"])
-                if fact:
-                    db.session.delete(fact)
-                    db.session.commit()
-                    return fact.to_dict()
-                else:
-                    return {"message": "subscription not found"}, 404
-            except Exception as e:
-                db.session.rollback()
-                return {"message": f"server error: {e}"}, 500
-
-
-
+             return {'message': f'Processed fact error'}, 210
+        
     class _Read(Resource):
         def get(self):
             facts = FactofDay.query.all()    # read/extract all users from database
